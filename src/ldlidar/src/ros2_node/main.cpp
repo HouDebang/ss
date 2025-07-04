ae90287903768551242d4ae6e41bed03f440b870
HouDengbang <1006958971@qq.com>
2025-07-04 11:20:20 +0800

    asdafdaf

--- src/ldlidar/src/ros2_node/main.cpp
+++ src/ldlidar/src/ros2_node/main.cpp
@@ -279,26 +279,26 @@ void  ToLaserscanMessagePublish(ldlidar::Points2D& src,  double lidar_spin_freq,
     lidarpub->publish(output);
     end_scan_time = start_scan_time;
 
-    // --- 新增：发布 base_link -> base_laser 的 tf 变换 ---
-    static std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster = nullptr;
-    if (!tf_broadcaster) {
-      tf_broadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(node);
-    }
-    geometry_msgs::msg::TransformStamped t;
-    t.header.stamp = output.header.stamp;
-    t.header.frame_id = "base_laser";
-    t.child_frame_id = "map";
-    t.transform.translation.x = -0.0398145505519817;
-    t.transform.translation.y = 0.0;
-    t.transform.translation.z = -0.04;
-    tf2::Quaternion q;
-    q.setRPY(0, 0, -1.5708);
-    t.transform.rotation.x = q.x();
-    t.transform.rotation.y = q.y();
-    t.transform.rotation.z = q.z();
-    t.transform.rotation.w = q.w();
-    tf_broadcaster->sendTransform(t);
-    // --- tf发布结束 ---
+//     // --- 新增：发布 base_link -> base_laser 的 tf 变换 ---
+//     static std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster = nullptr;
+//     if (!tf_broadcaster) {
+//       tf_broadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(node);
+//     }
+//     geometry_msgs::msg::TransformStamped t;
+//     t.header.stamp = output.header.stamp;
+//     t.header.frame_id = "base_laser";
+//     t.child_frame_id = "map";
+//     t.transform.translation.x = -0.0398145505519817;
+//     t.transform.translation.y = 0.0;
+//     t.transform.translation.z = -0.04;
+//     tf2::Quaternion q;
+//     q.setRPY(0, 0, -1.5708);
+//     t.transform.rotation.x = q.x();
+//     t.transform.rotation.y = q.y();
+//     t.transform.rotation.z = q.z();
+//     t.transform.rotation.w = q.w();
+//     tf_broadcaster->sendTransform(t);
+//     // --- tf发布结束 ---
   } 
 }
 
