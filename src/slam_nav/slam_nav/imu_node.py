0e0e291717bfd8f466f6e604e17f9caa8534431a
HouDengbang <1006958971@qq.com>
2025-07-05 01:42:59 +0800

    hgh

--- src/slam_nav/slam_nav/imu_node.py
+++ src/slam_nav/slam_nav/imu_node.py
@@ -23,8 +23,8 @@ class ImuNode(Node):
             return
         self.imu_pub = self.create_publisher(Imu, '/imu', 10)
         self.timer = self.create_timer(0.05, self.get_and_publish_imu)  # 20Hz
-        # self.tf_broadcaster = TransformBroadcaster(self)
-        # self.tf_timer = self.create_timer(0.1, self.publish_imu_tf)  # 10Hz
+        self.tf_broadcaster = TransformBroadcaster(self)
+        self.tf_timer = self.create_timer(0.05, self.publish_imu_tf)  # 20Hz
 
     def get_and_publish_imu(self):
         try:
@@ -73,19 +73,19 @@ class ImuNode(Node):
         except Exception as e:
             self.get_logger().warn(f"IMU数据解析失败: {e}")
 
-    # def publish_imu_tf(self):
-    #     t = TransformStamped()
-    #     t.header.stamp = self.get_clock().now().to_msg()
-    #     t.header.frame_id = "base_link"  # 发布的坐标系
-    #     t.child_frame_id = "base_imu_link"
-    #     t.transform.translation.x = 0.0  # 如有实际偏移请填写
-    #     t.transform.translation.y = 0.0
-    #     t.transform.translation.z = 0.0
-    #     t.transform.rotation.x = 0.0
-    #     t.transform.rotation.y = 0.0
-    #     t.transform.rotation.z = 0.0
-    #     t.transform.rotation.w = 1.0
-    #     self.tf_broadcaster.sendTransform(t)
+    def publish_imu_tf(self):
+        t = TransformStamped()
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = "base_link"  # 发布的坐标系
+        t.child_frame_id = "base_imu_link"
+        t.transform.translation.x = 0.0  # 如有实际偏移请填写
+        t.transform.translation.y = 0.0
+        t.transform.translation.z = 0.0
+        t.transform.rotation.x = 0.0
+        t.transform.rotation.y = 0.0
+        t.transform.rotation.z = 0.0
+        t.transform.rotation.w = 1.0
+        self.tf_broadcaster.sendTransform(t)
 
 def main(args=None):
     rclpy.init(args=args)
