993d1da917cacba53196ebe208dfed3055b9950d
HouDengbang <1006958971@qq.com>
2025-07-04 12:28:47 +0800

    wqdqd

--- src/fishbot_navigation2/launch/navigation2.launch.py
+++ src/fishbot_navigation2/launch/navigation2.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
     use_sim_time = launch.substitutions.LaunchConfiguration(
         'use_sim_time', default='false')
     map_yaml_path = launch.substitutions.LaunchConfiguration(
-        'map', default=os.path.join(fishbot_navigation2_dir, 'maps', 'room.yaml'))
+        'map', default=os.path.join(fishbot_navigation2_dir, 'maps', 'my_map.yaml'))
     nav2_param_path = launch.substitutions.LaunchConfiguration(
         'params_file', default=os.path.join(fishbot_navigation2_dir, 'config', 'nav2_params.yaml'))
 
