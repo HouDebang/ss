3e1e5d61ea9abf0bdba0e60185c3f70da2e30f74
HouDengbang <1006958971@qq.com>
2025-07-07 16:53:28 +0800

    238

--- src/fishbot_navigation2/launch/navigation2.launch.py
+++ src/fishbot_navigation2/launch/navigation2.launch.py
@@ -11,24 +11,31 @@ def generate_launch_description():
         'fishbot_navigation2')
     nav2_bringup_dir = get_package_share_directory('nav2_bringup')
     rviz_config_dir = os.path.join(
-        nav2_bringup_dir, 'rviz', 'nav2_default_view.rviz')
+        fishbot_navigation2_dir, 'rviz', 'nav2_default_view.rviz')
     
     # 创建 Launch 配置
-    use_sim_time = launch.substitutions.LaunchConfiguration(
-        'use_sim_time', default='false')
-    map_yaml_path = launch.substitutions.LaunchConfiguration(
-        'map', default=os.path.join(fishbot_navigation2_dir, 'maps', 'my_map.yaml'))
-    nav2_param_path = launch.substitutions.LaunchConfiguration(
-        'params_file', default=os.path.join(fishbot_navigation2_dir, 'config', 'nav2_params.yaml'))
+    use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time')
+    map_yaml_path = launch.substitutions.LaunchConfiguration('map')
+    nav2_param_path = launch.substitutions.LaunchConfiguration('params_file')
+ 
 
     return launch.LaunchDescription([
         # 声明新的 Launch 参数
-        launch.actions.DeclareLaunchArgument('use_sim_time', default_value=use_sim_time,
-                                             description='Use simulation (Gazebo) clock if true'),
-        launch.actions.DeclareLaunchArgument('map', default_value=map_yaml_path,
-                                             description='Full path to map file to load'),
-        launch.actions.DeclareLaunchArgument('params_file', default_value=nav2_param_path,
-                                             description='Full path to param file to load'),
+        launch.actions.DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='False',
+            description='Use simulation (Gazebo) clock if true'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            'map',
+            default_value=os.path.join(fishbot_navigation2_dir, 'maps', 'my_map.yaml'),
+            description='Full path to map file to load'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            'params_file',
+            default_value=os.path.join(fishbot_navigation2_dir, 'config', 'nav2_params.yaml'),
+            description='Full path to param file to load'
+        ),
 
         launch.actions.IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
